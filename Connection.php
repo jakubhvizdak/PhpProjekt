8aa5c9501bd0c50e2fa3a741924a5541fda2c41e
z19istc11 <z19istc11@z00p3-11.SPSKNM>
2022-02-23 11:36:47 +0100

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -11,7 +11,27 @@ $conn = new mysqli($servername, $username, $password,$dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully </br>";
+ // echo "Connected successfully </br>";
+
+
+ $sql = "SELECT id, username, password FROM users";
+ $result = mysqli_query($conn, $sql);
+print_r( mysqli_num_rows($result) );
+
+if (mysqli_num_rows($result) > 0 ) {
+  while($row = mysqli_fetch_assoc($result)){
+    echo "id: " . $row["id"]. " - Name: " . $row["username"]. " " . "<br>";
+  }
+} else {
+  echo "0 results";
+
+
+}
+
+
+
+
+
 
 
 ?>
